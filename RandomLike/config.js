23a3f64626138f46e9c664315e8883d6ff2a12c7
James Mc Dermott <james.mcdermott89@gmail.com>
2017-07-07 22:32:32 +0100

    Create config.js

---
+++ RandomLike/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'your consumer key',
+  consumer_secret: 'your consumer secret',
+  access_token: 'your access token',
+  access_token_secret: 'your access token secret'
+}
