46b7cdc4432de63a05c2a0ac8cd4f895823e7069
James Mc Dermott <james.mcdermott89@gmail.com>
2017-07-11 12:09:02 +0100

    Delete config.js

--- FollowBot/config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  consumer_key: 'HotlzNXcjvI2BhT1Ezqr71jbU',
-  consumer_secret: 'xWkC3umJvN59DH7MFZGmCuClsaoz3bVhngr1BfLuxUfXYPPxIq',
-  access_token: '1628991660-0U3iOvUo8Xxz4ZkACP94cjsAWyDzemnG4jPxhJL',
-  access_token_secret: '5Rjz9mj4fSkf0PE1XDvJ1PYkB3AArVaLtj9kfDIGBLyDm'
-}
